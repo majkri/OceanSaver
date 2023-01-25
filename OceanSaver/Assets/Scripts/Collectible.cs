b8e9b2403c93c96c5cb55a138593af8eaa05c829
MathildaMoller <up202202248@edu.fe.up.pt>
2023-01-25 19:06:56 +0000

    Counting the collectibles

--- OceanSaver/Assets/Scripts/Collectible.cs
+++ OceanSaver/Assets/Scripts/Collectible.cs
@@ -7,21 +7,39 @@ using UnityEngine;
 //The creation of the script starts at 5:35
 //In the tutorial the script is called "coin" but I named it ""Collectible"" to better fit our game 
 
+
+//To make the collectibles have a popup point the following tutorial was used: https://www.youtube.com/watch?v=esnaEYYD1ZM&t=148s&ab_channel=GameDevel
+
 public class Collectible : MonoBehaviour
+
+
 {
     public int CoinValue = 1; //It needs to be named "CoinValue" to fit to the ScoreManager script (I think)
+    public GameObject floatingPoints; //belongs to floating points
+
+    private float timer;
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             ScoreManager.instance.ChangeScore(CoinValue);
+            Instantiate(floatingPoints, transform.position, Quaternion.identity); //belongs to floating 
+            timer += Time.deltaTime;
+
+            if (timer > 1) Destroy(floatingPoints);
+           
             
         }
 
         if (other.gameObject.CompareTag("Border"))
         {
             Destroy(gameObject);
+            //Instantiate(floatingPoints, transform.position, Quaternion.identity); //belongs to floating points
         }
     }
+
+   
+
 }
